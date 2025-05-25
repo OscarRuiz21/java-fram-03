1195ccf4f8b5cce96a0535a9cc7b58f720e382bc
OscarRu <oscar.ruizhu@gmail.com>
2025-05-25 11:18:10 -0600

    adding eureka service

--- Basico/1_springboot-servicio[ribbon]/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/agencia/SpringbootServicioAgenciaApplication.java
+++ Basico/1_springboot-servicio[ribbon]/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/agencia/SpringbootServicioAgenciaApplication.java
@@ -2,12 +2,14 @@ package com.formacionbdi.springboot.app.agencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @RibbonClient(name = "servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient 
 public class SpringbootServicioAgenciaApplication {
 
 	public static void main(String[] args) {
